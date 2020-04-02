9aca28bda9bce25dbb7277edca00eac700a0c4cd
Akhil Stanislavose <akhil.stanislavose@gmail.com>
2020-04-01 20:46:09 -0700

    Day 4 Part 2

--- src/DayFour.hs
+++ src/DayFour.hs
@@ -19,11 +19,26 @@ readInt = read
 toDigits :: Show a => a -> [Int]
 toDigits = map readInt . tail . splitOn "" . show
 
+findPossiblePasswords :: Int -> Int -> [[Int]]
 findPossiblePasswords start end =
-  length
-    . filter isIncreasingOrSame
-    . filter hasAdjecent
-    . map toDigits
-    $ [start .. end]
+  filter isIncreasingOrSame . filter hasAdjecent . map toDigits $ [start .. end]
 
-run = findPossiblePasswords 145852 616942
+solvePartOne = length $ findPossiblePasswords 145852 616942
+
+group :: [[Int]] -> [Int] -> [[Int]]
+group ys []       = ys
+group [] (x : xs) = group [[x]] xs
+group ys (x : xs) = if head (last ys) == x
+  then group (init ys ++ [last ys ++ [x]]) xs
+  else group (ys ++ [[x]]) xs
+
+hasLengthTwo :: [a] -> Bool
+hasLengthTwo xs = length xs == 2
+
+hasOnePairAdjacentMatching :: [Int] -> Bool
+hasOnePairAdjacentMatching = any hasLengthTwo . group []
+
+solvePartTwo = length
+  $ filter hasOnePairAdjacentMatching (findPossiblePasswords 145852 616942)
+
+run = solvePartTwo
